e1706818054f482a565de935975a3ad003279d2e
Festim Krasniqi <106912999+FestimKrasniqi@users.noreply.github.com>
2023-05-14 16:15:02 +0200

    Update HelloController.java

--- src/main/java/com/example/hill_cipher/HelloController.java
+++ src/main/java/com/example/hill_cipher/HelloController.java
@@ -355,3 +355,113 @@ public class HelloController implements Initializable {
         encryptedTextOutput.setText(plaintext);
 
     }
+     private int[][] getMatrixFromInput() {
+        int[][] matrix = null;
+        switch (matrixSizeChoiceBox.getValue()) {
+            case 2:
+                setMatrixVisibility(false);
+                matrixInput2x2.setVisible(true);
+                matrix = new int[2][2];
+                matrix[0][0] = Integer.parseInt(matrix00.getText());
+                matrix[0][1] = Integer.parseInt(matrix01.getText());
+                matrix[1][0] = Integer.parseInt(matrix10.getText());
+                matrix[1][1] = Integer.parseInt(matrix11.getText());
+                break;
+            case 3:
+                matrix = new int[3][3];
+                setMatrixVisibility(false);
+                matrixInput3x3.setVisible(true);
+                matrix[0][0] = Integer.parseInt(matrix002.getText());
+                matrix[1][0] = Integer.parseInt(matrix012.getText());
+                matrix[2][0] = Integer.parseInt(matrix022.getText());
+                matrix[0][1] = Integer.parseInt(matrix102.getText());
+                matrix[1][1] = Integer.parseInt(matrix112.getText());
+                matrix[2][1] = Integer.parseInt(matrix122.getText());
+                matrix[0][2] = Integer.parseInt(matrix202.getText());
+                matrix[1][2] = Integer.parseInt(matrix212.getText());
+                matrix[2][2] = Integer.parseInt(matrix222.getText());
+                break;
+            case 4:
+                matrix = new int[4][4];
+                setMatrixVisibility(false);
+                matrixInput4x4.setVisible(true);
+                matrix[0][0] = Integer.parseInt(matrix003.getText());
+                matrix[1][0] = Integer.parseInt(matrix013.getText());
+                matrix[2][0] = Integer.parseInt(matrix023.getText());
+                matrix[3][0] = Integer.parseInt(matrix033.getText());
+                matrix[0][1] = Integer.parseInt(matrix103.getText());
+                matrix[1][1] = Integer.parseInt(matrix113.getText());
+                matrix[2][1] = Integer.parseInt(matrix123.getText());
+                matrix[3][1] = Integer.parseInt(matrix133.getText());
+                matrix[0][2] = Integer.parseInt(matrix203.getText());
+                matrix[1][2] = Integer.parseInt(matrix213.getText());
+                matrix[2][2] = Integer.parseInt(matrix223.getText());
+                matrix[3][2] = Integer.parseInt(matrix233.getText());
+                matrix[0][3] = Integer.parseInt(matrix303.getText());
+                matrix[1][3] = Integer.parseInt(matrix313.getText());
+                matrix[2][3] = Integer.parseInt(matrix323.getText());
+                matrix[3][3] = Integer.parseInt(matrix333.getText());
+                break;
+        }
+        return matrix;
+    }
+
+    private ArrayList<ArrayList<Integer>> getArrayListFromMatrix() {
+        ArrayList<ArrayList<Integer>> arrayList = new ArrayList<>();
+        int[][] matrix = getMatrixFromInput();
+        if (matrix != null) {
+            for (int[] row : matrix) {
+                ArrayList<Integer> list = new ArrayList<>();
+                for (int num : row) {
+                    list.add(num);
+                }
+                arrayList.add(list);
+            }
+        }
+        return arrayList;
+    }
+
+
+    public void setMatrixVisibility(boolean set){
+        matrixInput2x2.setVisible(set);
+        matrixInput3x3.setVisible(set);
+        matrixInput4x4.setVisible(set);
+    }
+
+    @FXML
+    private void goToDecrypt() throws IOException {
+        enter.setText("Enter cipher Text");
+        encryptButton.setText("Decrypt");
+        encryptButton.setOnAction(e->{
+            ArrayList<ArrayList<Integer>> matrix = getArrayListFromMatrix();
+            String ciphertext = plainTextInput.getText();
+            String plaintext = HillCipher.decryption(ciphertext, matrix);
+            encryptedTextOutput.setText(plaintext);
+        });
+    }
+
+    @FXML
+    private void DecryptButton() throws IOException{
+        DecryptBtn.setOnAction(e->{
+            ArrayList<ArrayList<Integer>> matrix = getArrayListFromMatrix();
+            String ciphertext = plainTextInput.getText();
+            String plaintext = HillCipher.decryption(ciphertext, matrix);
+            encryptedTextOutput.setText(plaintext);
+        });
+    }
+    @FXML
+    private void goToEncrypt() throws IOException {
+        enter.setText("Enter plain text:");
+        encryptButton.setText("Encrypt");;
+        encryptButton.setOnAction(e->{
+            int[][] matrix = getMatrixFromInput();
+            String plainText = plainTextInput.getText();
+            String encryptedText = HillCipher.encrypt(plainText, matrix);
+            encryptedTextOutput.setText(encryptedText);
+        });
+
+    }
+
+
+
+}
