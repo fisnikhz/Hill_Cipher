6bfdadbc62ea994358a8eeecd8fe53cc8a74d4d0
Ferdeze Nurkasa <ferdeze.nurkasa@student.uni-pr.edu>
2023-05-14 17:00:38 +0200

    title

--- src/main/java/com/example/hill_cipher/Application.java
+++ src/main/java/com/example/hill_cipher/Application.java
@@ -11,7 +11,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Application_View.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400 ,400 );
-        stage.setTitle("Hello!");
+        stage.setTitle("Hill Cipher");
         stage.setScene(scene);
         stage.show();
     }
