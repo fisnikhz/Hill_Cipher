5069ac9b683abcc8bf187668c060fe10994ae624
Ferdeze Nurkasa <ferdeze.nurkasa@student.uni-pr.edu>
2023-05-14 16:17:11 +0200

    Main

--- src/main/java/com/example/hill_cipher/HelloApplication.java
+++ src/main/java/com/example/hill_cipher/HelloApplication.java
@@ -1,4 +1,4 @@
-package com.example.hill_cipher;
+package com.example.demo112;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 400 ,400 );
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
